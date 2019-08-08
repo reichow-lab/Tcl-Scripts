34e7a698f221ca675ef299a00f8bd786895d99cf
Bassam Haddad <bhaddad@pdx.edu>
2019-08-07 18:18:16 -0700

    edited

--- LipNetwork.tcl
+++ LipNetwork.tcl
@@ -18,41 +18,33 @@
 #
 #		LipCenters.txt	- This input will contain the info for the location and "ID's" of the lipids around the protein...Unfortunately there will be 12 sections,
 #				each with multiple (equivalent) lipid centers.
-#		IsoValueRange	- This will just be two values passed either as an argument to running a proc (likely) are something hard-coded into the script...essentially
-#				it provides the range of CryoEM density that we are using to isolate the lipids that are occupying the space.
-#		PercentWithin	- This value (0 - 100) will determine the minimum criteria for a lipid-tail to be counted as within a density...i.e. if PercentWithin is set
-#				to 57, then at least 57% of the atoms of a lipid tail must be within the density for the tail to be marked as within the density. The higher
-#				the value, the more strict the results.
+#		Output_name	- This is the name of the file that the final connectivity matrix will be written to...It is written in a simplistic space delimited
+#				text-file, which will have an accompanying python script to make it into a numpy array.
+#		IsoLow		- This is the minimum isovalue (density of volume map) that each lipid tail must occupy (on average) in order for the tail to be 
+#				considered occupying a lipid density. This is a tuneable parameter...a lower isovalue accepts more lipids.
 #
-#	Processes:
+#	Processes to come:
 #
-#	get-centers	- This proc() will take a text file with the the geometric (x,y) coordinates of the lipid densities around the protein.
-#			The centers will need to be collected by hand (for now). These centers will be the basis for locating where the lipid densities are.
-#
-#	get-lipids	- Creates a list of the lipids that will be the subject of analysis. Needs Segid & Resid for each lipid. This will also contain the information
-#			regarding whether or not it is POPC or DMPC...Since we mostly care about the tails.
-#			- This can first make an atom selection based off of any lipids that are within a certain iso-value at anypoin in the simulation, either that or a
-#			distance based procedure...which would probably be faster, however less exact?
-#
-#	lip-analysis	- Goes through each lipid in the list, checking where their tails are, and checking if a tail meets the criteria of being within a density...
-#
-#	pop-matrix	- This proc() will be called by Lip-Analysis to populate a transition matrix of lipid tail interactions (network config)
-#
-#	rep-lipids	- This proc() will be called by Lip-Analysis to keep track of which lipids & frames show a lipid with its tails in the densities.
-#
-#	which_center	- This evaluates which density the lipids  are closest to, and returns the density ID
-#
-#	eval_density	- Evaluates the average density that the lipid-tail occupys...if the avg density is >= Iso_Low, then it returns true.
+#		rep_lipids()	- Takes a snapshot of a lipid occupying two densities, demonstrating connectivity. Alternatively, it could also just spit out a list of the
+#				[resid - segid - frame] that the snapshot occurs, this way it doesn't spit out a shitton of .pdb's.
 #
 ################################
 source	~/Scripts/TCL/Tcl-Scripts/matrix.tcl
+#source	~/Scripts/TCL/Tcl-Scripts/Lip-Align_movie.tcl
 ################################
-#
 #
 proc LipNetwork		{infile outfile IsoVal} {
 
 #	This is the main program which is what will be called from the TK-Console.
 #	It initalizes the matrix by inserting rows/colums, as well as starts the analysis.
+#	variables:
+#	
+#		LipDict	:	Dictionary containing (x,y,lipID) of each lipid density, pulls data from LipCenters.txt
+#		LipMat	:	The pseudo-transition matrix (connectivity matrix) linked to LipArr
+#		LipArr	:	The array that the matrix is linked to, allowing me to easily call values from the matrix
+#		IsoLow	:	Isovalue that is threshold for judging occupancy
+#		NumFrames	Number of frames in the simulation
+#		DenNum	:	Number of lipid densities (centers)	
 
 	global	LipDict LipMat LipArr IsoLow NumFrames DenNum
 
@@ -101,9 +93,8 @@ proc get_centers	{infile} {
 
 	set	centers		[split $centers_read "\n"]
 
-#	set	DenNum		[expr [expr [llength $centers] - 1] / 12] 
-	set	DenNum		13
-
+	set	DenNum		[expr [llength $centers]]
+	puts	"DenNum: $DenNum"
 	set	i		0
 
 	foreach line $centers {
@@ -153,9 +144,9 @@ proc lip_analysis	{} {
 	
 	set tail_1_text "lipid and (name C22 to C29 C210 to C214)"
 	set tail_2_text "lipid and (name C32 to C39 C310 to C316)"
-
+	puts "debug 1"
 	set ind_percent [expr {round([llength $Phos_Ind] / ([expr [llength $Phos_Ind] / 20]))}]
-	
+	puts "debug 2"	
 	set i		0
 
 	foreach index $Phos_Ind {
@@ -240,9 +231,9 @@ proc eval_density	{lipid_tail} {
 
 		set tot_den	[expr $tot_den + [$lip_atom get interpvol0]]
 	}
-
+	puts "debug at avg_den"
 	set avg_den		[expr $tot_den / [llength $lipid_index]]
-
+	puts "debug past avg_den"
 	if {$avg_den >= $IsoLow} {	
 
 		return true 
@@ -270,18 +261,19 @@ proc pop_matrix		{den_id_1 den_id_2 {writematrix false} {outfile false}} {
 
 	# row: j, column: i ... tcl matrix object has the form (column,row) which is distinct from matrices in python
 
-		for {set i 0} {$j < $DenNum} {incr i} {
+		for {set j 0} {$j < $DenNum} {incr j} {
 	
-			for {set j 0} {$i < $DenNum} {incr j} {
+			for {set i 0} {$i < $DenNum} {incr i} {
 
 				set	val	$LipArr($i,$j)
 
-				puts	-nonewline	$outf	"$Val"
+				puts	-nonewline	$outf	"$val\t"
 			}
 
 			puts	$outf	"\n"
 		} 
 
+		close	$outf
 	}
 	
 }
