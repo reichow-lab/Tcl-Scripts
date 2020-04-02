06eef0416564d81cc2bcf37070ab73e0448ea184
Bassam Haddad <bhaddad@pdx.edu>
2020-04-01 21:45:58 -0700

    finished Ion_BinCount_new.tcl

--- Ion_BinCount_new.tcl
+++ Ion_BinCount_new.tcl
@@ -21,25 +21,41 @@ proc run {ofile zmin zmax holeinput} {
 	set output [open $ofile w]
 
 	# hole file has position along pore (index: 0) and radius (index: 1)
+        # create dictionary of (pore-axis, radius) pairs
 
 	set holfile	[open $holeinput r]
+	set holeread	[read -nonewline $holefile]
+	set holinputs	[split $holeread "\n"]
+	close holefile
+
+	foreach line $holinputs {
+
+	set pore	[lindex $line 0]
+	set radius	[lindex $line 1]
+
+	set holedict	[dict set holedict $pore $radius]
+	}
 
 ## Loops through each of the frames to get a count at each timestep
 
+	set range	[expr $zmax - $zmin]
+
 	for {set i 0} {$i < [expr $numframes + 1]} {incr i} {
 
-		animate goto $i
+		animate goto 		$i
 
-		set pot [atomselect top "name POT and ((z > $zmin and z < $zmax) and (x^2 + y^2 < 270))"]
-                set cla [atomselect top "name CLA and ((z > $zmin and z < $zmax) and (x^2 + y^2 < 270))"]
+		set 	num_pot		0
+		set	num_cla		0
 
-		set num_pot [$POT num]
-		set num_cla [$CLA num]
+		for {set j $zmax} {$j > $zmin} {set j [expr j - 1]} {
+
+			set pot [atomselect top "name POT and ((z > [expr $j - 1] and z < $j) and (x^2 + y^2 < [dict get $holedict $j]^2))"]
+	                set cla [atomselect top "name CLA and ((z > [expr $j - 1] and z < $j) and (x^2 + y^2 < [dict get $holedict $j]^2))"]
+			
+			set num_pot	[expr $num_pot + [$pot num]]	
+			set num_cla	[expr $num_cla + [$cla num]]
 
 		puts $output "$num_pot $num_cla"
-
 	}
-
 	close $output
-
 }
