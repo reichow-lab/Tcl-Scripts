4b05fa116d4940566434f1a10f4428bb2becd3e4
Bassam Haddad <bhaddad@pdx.edu>
2020-04-03 17:53:59 -0700

    finished Ion_BinCount_new.tcl

--- Ion_BinCount_new.tcl
+++ Ion_BinCount_new.tcl
@@ -8,37 +8,33 @@
 ##########################################################################
 
 
-puts "Type the following to run program: 'run <output name> <zmin> <zmax> <holeinput>'"
+puts "Type the following to run program: 'run <output name> <zmin> <zmax> <binsize>'"
 
-proc run {ofile zmin zmax holeinput} {
+proc run {ofile zmin zmax} {
 
 	global ion_name
 
+	# align pore to the center of the system
+
 	set prot [atomselect top protein]
+
+	set prot_cen [measure center $prot]
+
+	set x	[expr [lindex $prot_cen 0] * -1]
+	set y 	[expr [lindex $prot_cen 1] * -1]
+	set z	[expr [lindex $prot_cen 2] * -1]
+	
+	set vec	[list $x $y $z]
+
+	$prot moveby $vec
 
 	set numframes [molinfo top get numframes]
 
 	set output [open $ofile w]
 
-	# hole file has position along pore (index: 0) and radius (index: 1)
-        # create dictionary of (pore-axis, radius) pairs
-
-	set holefile	[open $holeinput r]
-	set holeread	[read -nonewline $holefile]
-	set holinputs	[split $holeread "\n"]
-	close $holefile
-
-	foreach line $holinputs {
-
-	set pore	[format %.0f [lindex $line 0]]
-	set radius	[lindex $line 1]
-	
-	set holedict	[dict set holedict $pore $radius]
-	}
-	
 	## Loops through each of the frames to get a count at each timestep
 
-	for {set i 0} {$i < [expr $numframes + 1]} {incr i} {
+	for {set i 0} {$i < [expr $numframes]} {incr i} {
 
 		animate goto 		$i
 
@@ -47,8 +43,9 @@ proc run {ofile zmin zmax holeinput} {
 
 		for {set j $zmax} {$j > $zmin} {set j [expr $j - 1]} {
 
-			set pot [atomselect top "name POT and ((z > [expr $j - 1] and z < $j) and (x^2 + y^2 < [dict get $holedict $j]^2))"]
-	                set cla [atomselect top "name CLA and ((z > [expr $j - 1] and z < $j) and (x^2 + y^2 < [dict get $holedict $j]^2))"]
+			set pot [atomselect top "name POT and ((z > [expr $j - 1] and z < $j) and (x^2 + y^2 < (25)^2))"]
+	                set cla [atomselect top "name CLA and ((z > [expr $j - 1] and z < $j) and (x^2 + y^2 < (25)^2))"]
+			set b [expr $j -1]
 			
 			set num_pot	[expr $num_pot + [$pot num]]	
 			set num_cla	[expr $num_cla + [$cla num]]
