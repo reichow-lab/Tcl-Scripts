d3a02eaf9ea0cdc86608172e61a9770380495c46
Bassam Haddad <bhaddad@pdx.edu>
2020-07-12 16:27:53 -0700

    fixed variable naming

--- Lip-Analysis-Tools.tcl
+++ Lip-Analysis-Tools.tcl
@@ -2,7 +2,6 @@ source	~/Scripts/TCL/Tcl-Scripts/calc_op_module.tcl
 source	~/Scripts/TCL/Tcl-Scripts/calc_op.tcl
 
 proc	Title	{{v ""}} {
-
 	if {$v == "-v"} {
 		puts		""
 		puts		"Proc Lip-align:	Takes the users selection of any lipids, and aligns them together to a reference lipid.\n"
@@ -14,10 +13,10 @@ proc	Title	{{v ""}} {
 		puts		"Proc Prot-align:	Takes all of the annular lipids around the protein, and aligns them to a single protein chain.\n"
 		puts		"			To run '$ Prot-align <MOLID> <LOGFILE-NAME> <CHAIN>'\n"
 		puts            "Proc LipNetwork:	Using the centers file, containing the (x,y) coordinates of the annular\n"
-                puts            "			lipid densities (from MD or CryoEM) and calculates the interconnectivity\n" 
+                puts            "			lipid densities (from MD or CryoEM) and calculates the interconnectivity\n"
                 puts            "			of the lipid tails within the densities. This script creates a 'transition\n"
                 puts            "			matrix' that shows when any two lipid densities are occupied by a single\n"
-                puts            "			lipid. The default minimum IsoLow is 0, meaning the density does not matter\n" 
+                puts            "			lipid. The default minimum IsoLow is 0, meaning the density does not matter\n"
                 puts            "			and thus assumed that a tail is always assigned to a lipid density 'region'.\n"
                 puts            "			In order for this program to work, the volume file (.mrc or .dx) must be\n"
                 puts            "			loaded into the top molecule (the one containing your .dcd. Furthermore, \n"
@@ -26,7 +25,6 @@ proc	Title	{{v ""}} {
 		puts            "			To run '$ LipNetwork <CENTER FILE> <OUTFILE> <CARBON-THRESHOLD> <ISO-THRESHOLD (default: none)> <DIFSEL (default: false)>'\n"
 
 	} else {
-
 		puts		""
 		puts            "			To run '$ Lip-align <MOLID>'\n"
 		puts            "			To run '$ align'\n"
@@ -36,239 +34,140 @@ proc	Title	{{v ""}} {
 		puts		"			To run '$ PerLipidOP <outname>'\n"
 	}
 }
-
-
 # Aligns lipids together to show how similar/different they are. Demonstrates the range of motion,
 # but does not preserve information on their contact with the protein.
-
 proc	Lip-align	{MOLID} {
-
 	set	AtomList	[label list Atoms]
-
 	set	NumFrames	[molinfo $MOLID get numframes]
-
 	set	ref_lip		[atomselect $MOLID "same residue as index [lindex $AtomList {0 0 1}]"]
-
 	set	n		0
-
 	foreach	line	$AtomList {
-
 		set	Index		[lindex $line {0 1}]
-		
 		set	align_lip	[atomselect top "same residue as (index $Index)"]
-
 		for {set i 0} {$i < $NumFrames} {incr i} {
-
 			animate goto $i
-
 			set trans_matrix	[measure fit $align_lip $ref_lip]
-
 			$align_lip move $trans_matrix
-
 		}
-
 	}
 	Title
 }
 
-
 proc align {} {
-
 	puts -nonewline " Provide molID for reference."
 	flush stdout
-
 	set ref_molid [gets stdin]
-
 	puts -nonewline " Provide molID for selection."
 	flush stdout
-
 	set sel_molid [gets stdin]
-
 	set numframes [molinfo $sel_molid get numframes]
-
 	set ref_frame [atomselect $ref_molid "protein and name CA" frame 0]
-
 	set n 1
-
 	set sys [atomselect $sel_molid all]
-
 	set frame_percent [expr {round($numframes / 30)}]
-
 	for {set i 0} {$i < $numframes} {incr i} {
-
                 animate goto $i
-
                 set align_frame [atomselect $sel_molid "protein and name CA"]
-
                 set trans_matrix [measure fit $align_frame $ref_frame]
-
                 $sys move $trans_matrix
-
 		if {($i % $frame_percent) == 0 } {
-
 			puts -nonewline "*"
 			flush stdout
 		}
-
         }
-
         puts  "\nAlignments complete, ready for RMSD calculations"
 	Title
 }
 
-
-
 proc	Prot-align	{MOLID logfile {RefChain A}} {
-
 	set	log	[open $logfile w]
 	puts	$log	"RESID\t\tSEGID\t\tLip-Head RMSF\t\tLip-Tail RMSF\t\tLip-Tot RMSF\n"
-
 	# Select the protein chain (connexin) that will be the reference for transformation
-
-	set ProtChainList	[lsort -unique [[atomselect $MOLID protein] get chain]]	
-
+	set ProtChainList	[lsort -unique [[atomselect $MOLID protein] get chain]]
 	set NumFrames		[molinfo $MOLID get numframes]
-
 	set ref_prot		[atomselect $MOLID "protein and chain $RefChain frame 0"]
-
 	foreach chain $ProtChainList {
-
 		set	Phos_List	""
-
 		set	align_prot	[atomselect $MOLID "protein and chain $chain"]
-
 		for {set n 0} {$n < $NumFrames} {incr n} {
-			
 			animate goto $n
-
 			set	Prot_Lip	[atomselect $MOLID "resname DMPC and same residue as within 10 of (protein and (resid 84 215) and chain $chain)"]
-
 			$Prot_Lip		set beta 1
-
 			set	hold		[[atomselect $MOLID "name P and beta = 1"] get index]
-
 			set	Phos_List	[concat	[lindex $Phos_List] [lindex $hold]]
-		
 			$Prot_Lip		set beta 0
-
 			unset	hold
 		}
-
 		set Phos_Ind	[lsort -unique $Phos_List]
-
 		set Lip_Sel	[atomselect $MOLID "same residue as index [lindex $Phos_Ind]"]
-
 		unset Phos_List
-
 		for {set i 0} {$i < $NumFrames} {incr i} {
-
 			animate goto $i
-
 			set trans_matrix	[measure fit $align_prot $ref_prot]
-
 			$Lip_Sel move $trans_matrix
 		}
-
 		foreach phos $Phos_Ind {
-
 			set	IND	[atomselect $MOLID "index $phos"]
-
 			set	ResID	[$IND get resid]
 			set	SegID	[$IND get segid]
-
 			set	LipTot	[atomselect $MOLID "resid $ResID and segid $SegID"]
 			set	LipTail	[atomselect $MOLID "resid $ResID and segid $SegID and (name C22 to C29 C210 to C214 C32 to C39 C310 to C314)"]
 			set	LipHead	[atomselect $MOLID "resid $ResID and segid $SegID and (name O21 O22 O31 O32 O11 to O14 C1 C2 C21 C3 C31 C11 to C15 P N)"]
-
 			set	Headr	[measure rmsf $LipHead]
 			set	Tailr	[measure rmsf $LipTail]
 			set	Totar	[measure rmsf $LipTot]
-			
 			set	headhold	0
 			set	tailhold	0
 			set	totalhold	0
-
 			foreach	head $Headr {set headhold	[expr $headhold + $head]}
 			foreach tail $Tailr {set tailhold	[expr $tailhold + $tail]}
 			foreach total $Totar {set totalhold	[expr $totalhold + $total]}
-	
 			set	HeadrA		[expr $headhold / [llength $Headr]]
 			set	TailrA		[expr $tailhold / [llength $Tailr]]
 			set	TotarA		[expr $totalhold / [llength $Totar]]
-
 		puts	$log	"$ResID\t\t$SegID\t\t$HeadrA\t\t$TailrA\t\t$TotarA\n"
 		}
-
 		puts -nonewline "*"
-
 	}
 	close	$log
-
 	puts "*"
-
 	Title
 }
-
 proc	PerLipidOP {outname} {
-
 	set	lipids	[atomselect top "lipids and name P"]
-
 	set	num_lip	[$lipids num]
-
 	set	ResList	[$lipids get resid]
-
 	set	SegList	[$lipids get segid]
-
 	set	k	1
-
 	set	prot	[atomselect top protein]
 	set	prot_x	[lindex	[measure center $prot]	0]
 	set	prot_y	[lindex [measure center $prot]	1]
-
 	foreach segid $SegList resid $ResList {
-	
 		set	lipid	[atomselect top "resid $resid and segid $segid"]
 		set	lip_x	[lindex [measure center $lipid]	0]
 		set	lip_y	[lindex [measure center $lipid] 1]
-
 		set	radius	[expr {sqrt(pow(($lip_x - $prot_x),2) + pow(($lip_y - $prot_y),2))}]
-
 		orderparam-c2	arr2	"resid $resid and segid $segid"
-		
 		set		listc2	""
-
 		foreach {carbon	parval}	[array get arr2] {
-			
 			lappend	listc2	"$carbon $parval"
 		}
-
 		orderparam-c3	arr3	"resid $resid and segid $segid"
-
 		set		listc3	""
-
 		foreach {carbon parval} [array get arr3] {
-
 			lappend	listc3	"$carbon $parval"
 		}
-
 		set	sum2	0
 		set	sum3	0
-
 		for {set i 0} {$i <= 12} {incr i} {
-
 # Only averaging carbons 3 to 11 (appear in 'flat' region of figure), thus only looking at contributions of indices i -> 9
-
 			if {$i >= 1 && $i <= 9} {
-
 				set	sum2	[expr [lindex $listc2 $i 1] + $sum2]
 				set	sum3	[expr [lindex $listc3 $i 1] + $sum3]
 			}
-
 			if {$i == 12} {
-			
 				set	avg2		[expr $sum2 / 9]
 				set	avg3		[expr $sum3 / 9]
-
 				dict	set	OParam	$k	RESID	$resid
 				dict	set	OParam	$k	SEGID	$segid
 				dict	set	OParam	$k	C2OP	$listc2
@@ -281,190 +180,115 @@ proc	PerLipidOP {outname} {
 			}
 		}
 	}
-
 	animate	goto	0
-
 	set	all	[atomselect top all]
-
 	$all	set	beta	0
-
 	set	out	[open $outname w]
-
 	for {set i 1} {$i < $k} {incr i} {
-
 		set	AcylC2	[atomselect top "resid [dict get $OParam $i RESID] and segid [dict get $OParam $i SEGID] and (name C22 to C29 C210 to C214)"]
-
 		set	AcylC3	[atomselect top "resid [dict get $OParam $i RESID] and segid [dict get $OParam $i SEGID] and (name C32 to C39 C310 to C314)"]
-		
 		$AcylC2	set	beta	[dict get $OParam $i C2Avg]
-
 		$AcylC3 set	beta	[dict get $OParam $i C3Avg]
-
 		puts	$out	"$resid\t$segid\t$avg2\t$avg3\t$radius\n"
 	}
-
 	$all	writepdb	$outname.pdb
-
 	$all	writepsf	$outname.psf
-
 	close	$out
 }
-
 proc	SymLipOP {outname dr dmax zmaxU zminL} {
-
 #	dmax must be a multiple of dr
-
-	set num_shell	[expr $dmax / $dr] 
-
+	set num_shell	[expr $dmax / $dr]
 	set c		0
-
 	# isolate shells of lipids that are dr angstroms thick (only affecting the beta-value of the phosphates)
-
 	for {set i $num_shell} {$i >= 1} {set i [expr $i - 1]} {
-
-		set	shell_upper	[atomselect top "lipids and name P and (z > $zmaxU or z < $zminL) and within [expr $dmax - [expr $c * $dr]] of protein"] 
+		set	shell_upper	[atomselect top "lipids and name P and (z > $zmaxU or z < $zminL) and within [expr $dmax - [expr $c * $dr]] of protein"]
 		set	shell_lower	[atomselect top "lipids and name P and (z < $zmaxU and z > $zminL) and within [expr $dmax - [expr $c * $dr]] of protein"]
-
 		$shell_upper set	beta	$i
 		$shell_lower set	beta	$i
-
 		incr	c
 	}
-
 	# Select the lipids for the upper and lower leaflets
-
 	for {set i $num_shell} {$i >= 1} {set i [expr $i - 1]} {
-
 		set	shell_upper_($i)	[atomselect top "lipids and name P and (z > $zmaxU or z < $zminL) and beta = $i"]
 		set	shell_lower_($i)	[atomselect top "lipids and name P and (z < $zmaxU and z > $zminL) and beta = $i"]
-
 		set	upper_resid_($i)	[$shell_upper_($i) get resid]
 		set	upper_segid_($i)	[$shell_upper_($i) get segid]
-
 		set	lower_resid_($i)	[$shell_lower_($i) get resid]
 		set	lower_segid_($i)	[$shell_lower_($i) get segid]
 	}
-
 	# Calculate the average Scd Order Parameter for each shell/leaflet
-
 	for {set i $num_shell} {$i >= 1} {set i [expr $i - 1]} {
-
 		set	sumU	0
 		set	sumL	0
 		set	U	0
 		set	L	0
-
 		# Sum all of the beta fields from each lipid-tail carbon within a shell/leaflet, and keep track of the number of carbons being summed
-
 		foreach resid $upper_resid_($i) segid $upper_segid_($i) {
-
 			set	tail_c	[atomselect top "resid $resid and segid $segid and (name C22 to C29 C210 C211 C32 to C39 C310 C311)"]
 			set	CD_list	[$tail_c get beta]
-
-			foreach val $CD_list { 
-				
+			foreach val $CD_list {
 				set	sumU	[expr $sumU + $val]
-				
 				incr	U
 			}
-
 			unset	tail_c CD_list
 		}
-
-		if {$U != 0}	{set	avgU_($i)	[expr $sumU / $U]} 
-		 
+		if {$U != 0}	{set	avgU_($i)	[expr $sumU / $U]}
 		foreach resid $lower_resid_($i) segid $lower_segid_($i) {
-
 			set	tail_c	[atomselect top "resid $resid and segid $segid and (name C22 to C29 C210 C211 C32 to C39 C310 C311)"]
 			set     CD_list [$tail_c get beta]
-
 			foreach val $CD_list {
-
 				set     sumL    [expr $sumL + $val]
-
 				incr	L
 			}
-
 			unset	tail_c CD_list
 		}
-
 		if {$L != 0}	{set	avgL_($i)	[expr $sumL / $L]}
-
 		# Colour the lipids (whole) by the average beta-values of the lipid-tails
-
 		foreach residU $upper_resid_($i) segidU $upper_segid_($i) {
-
 		       #set	upper	[atomselect top "resid $residU and segid $segidU and ((name C22 to C29 C210 to C214) or (name C32 to C39 C310 to C314))"]
 			set	upper	[atomselect top "resid $residU and segid $segidU"]
-
 			$upper	set	beta	$avgU_($i)
 		}
-
 		foreach residL $lower_resid_($i) segidL $lower_segid_($i) {
-
 		       #set	lower	[atomselect top "resid $residL and segid $segidL and ((name C22 to C29 C210 to C214) or (name C32 to C39 C310 to C314))"]
-			set	lower	[atomselect top "resid $residL and segid $segidL"]	
-
+			set	lower	[atomselect top "resid $residL and segid $segidL"]
 			$lower	set	beta	$avgL_($i)
 		}
 	}
-
 	set	all	[atomselect top all]
-
 	$all	writepsf	$outname.psf
 	$all	writepdb	$outname.pdb
 }
 
 proc	RadLipOP {outname dr dmax zmaxU zminL} {
-
 	#This splits the surrounding lipids into shells of thickness dr, out to a distance dmax. zmaxU/2 are bounds for where the inner and outer leaflets
-	# are located. 
-
+	# are located.
 	set	num_shell	[expr $dmax / $dr]
-
 	set	c	0
-	# Starting with the outermost shell allows us to assign beta values within each shell due to the way atomselections work...if we start from 
+	# Starting with the outermost shell allows us to assign beta values within each shell due to the way atomselections work...if we start from
 	# shell-1 then we will constantly re-write the beta-values of the shells near the center.
-	
 	for {set i $num_shell} {$i >= 1} {set i [expr $i - 1]} {
-
 		set	shell_upper($i)	[atomselect top "lipids and name P and (z > $zmaxU or z < $zminL) and within [expr $dmax - [expr $c * $dr]] of protein"]
-
 		set	shell_lower($i) [atomselect top "lipids and name P and (z < $zmaxU and z > $zminL) and within [expr $dmax - [expr $c * $dr]] of protein"]
-
 		incr	c
 	}
-
 	for {set i $num_shell} {$i >= 1} {set i [expr $i - 1]} {
-
 		$shell_upper($i) set beta $i
 		$shell_lower($i) set beta [expr $i + 10]
 	}
-
 	for {set i 0} {$i < 8} {incr i} {
-
 		set	a	[expr $i * $dr]
 		set	b	[expr $a + $dr]
-
 #		set	($a)to($b)_upper	"all and same residue as beta = [expr $i + 1]"
 #		set	($a)to($b)_lower	"all and same residue as beta = [expr $i + 11]"
-
 		puts	"Starting $a -> $b upper sn2"
-		
 		orderparam-c2m arr1 "all and same residue as beta = [expr $i + 1]" ($a)to($b)_upper_sn2
-
 		puts	"Starting $a -> $b upper sn3"
-
 		orderparam-c3m arr2 "all and same residue as beta = [expr $i + 1]" ($a)to($b)_upper_sn3
-
 		puts	"Starting $a -> $b lower sn2"
-
 		orderparam-c2m arr3 "all and same residue as beta = [expr $i + 11]" ($a)to($b)_lower_sn2
-
 		puts	"Starting $a -> $b lower sn3"
-
 		orderparam-c3m arr4 "all and same residue as beta = [expr $i + 11]" ($a)to($b)_lower_sn3
-
 		unset	arr1 arr2 arr3 arr4
 	}
 }
--- LipNetwork.tcl
+++ LipNetwork.tcl
@@ -1,6 +1,6 @@
 # Program:	LipNetwork.tcl
 # Author:	Bassam Haddad
-# 
+#
 # Portland State University
 # Reichow Lab
 #
@@ -20,8 +20,8 @@
 #				each with multiple (equivalent) lipid centers.
 #		Output_name	- This is the name of the file that the final connectivity matrix will be written to...It is written in a simplistic space delimited
 #				text-file, which will have an accompanying python script to make it into a numpy array.
-#		IsoLow		- This is the minimum isovalue (density of volume map) that each lipid tail must occupy (on average) in order for the tail to be 
-#				considered occupying a lipid density. This is a tuneable parameter...a lower isovalue accepts more lipids. Two alternatives to entering a 
+#		IsoLow		- This is the minimum isovalue (density of volume map) that each lipid tail must occupy (on average) in order for the tail to be
+#				considered occupying a lipid density. This is a tuneable parameter...a lower isovalue accepts more lipids. Two alternatives to entering a
 #				value are: 'none', and 'sep'. 'none' takes away the density dependence and instead assignes every lipid to a center, whereas 'sep' tells
 #				LipNetwork to use center-specific Isolows which are in the CentersFile.
 #
@@ -30,339 +30,207 @@
 source	~/Scripts/TCL/Tcl-Scripts/Lip-Analysis-Tools.tcl
 source	~/Scripts/TCL/Tcl-Scripts/Animate_Lipid.tcl
 ################################
-
 Title
 
 proc LipNetwork		{infile outfile CarbonThreshold {IsoVal "none"} {difsel false}} {
-
 #	This is the main program which is what will be called from the TK-Console.
 #	It initalizes the matrix by inserting rows/colums, as well as starts the analysis.
 #	variables:
-#	
+#
 #		LipDict	:	Dictionary containing (x,y,lipID) of each lipid density, pulls data from LipCenters.txt
 #		LipMat	:	The pseudo-transition matrix (connectivity matrix) linked to LipArr
 #		LipArr	:	The array that the matrix is linked to, allowing me to easily call values from the matrix
 #		IsoLow	:	Isovalue that is threshold for judging occupancy
 #		NumFrames	Number of frames in the simulation
-#		DenNum	:	Number of lipid densities (centers)	
+#		DenNum	:	Number of lipid densities (centers)
 #		MinCarbon	Minimumm number of carbons required for classification
-
 	global	LipDict LipMat LipArr LipList IsoLow NumFrames DenNum MinCarbon OUTFILE
-
 	::struct::matrix	LipMat
-
 	array	set		LipArr	{}
-
 	LipMat	link		LipArr
-
 	set	LipList		""
-
 	set	molid		[molinfo top]
-
 	set	NumFrames	[molinfo top get numframes]
-
 	set	IsoLow		$IsoVal
-
 	set	MinCarbon	$CarbonThreshold
-
 	set	OUTFILE		$outfile
-
 	set	ReturnList	[get_centers $infile]
-
 	set	LipDict		[lindex $ReturnList 0]
-
 	set	DenNum		[lindex $ReturnList 1]
-	
 	for	{set i 0} {$i < $DenNum} {incr i} {
-
 		LipMat	insert	column	$i
 		LipMat	insert	row	$i
 	}
-
 	get_lipid_list
-	
 	lip_analysis $difsel
-
 	pop_matrix 0 0 true $outfile
-
 	if {$IsoLow != "none"} {
-
 		set LipLogOutname	"$OUTFILE[set ender "_LipList.log"]"
-
 		set LipLogOut		[open	$LipLogOutname w]
-
 		puts	$LipLogOut	"ResID\tSegID\tLipCenter-1\tLipCenter-2\tFrame\tCarbonTail-1\tCarbonTail-2"
-
 		foreach lipid $LipList {
-
 			puts	$LipLogOut	"[lindex $lipid 0]\t[lindex $lipid 1]\t[lindex $lipid 2]\t\t[lindex $lipid 3]\t\t[lindex $lipid 4]\t[lindex $lipid 5]\t\t[lindex $lipid 6]"
-
 		}
-
 		close	$LipLogOut
-
 	}
-
 	puts	"final matrix written to $outfile"
-
 	LipMat	destroy
-
 	Title
 }
 
 proc get_centers	{infile} {
-
 #	This proc() takes a text file that contains the (x,y) coordinates of the lipid densities that are resolved by CryoEM (or any lipid density).
-#	These coordinates are used to fill a dictionary with the (x,y) pair, and the lipid ID (arbitrarily defined); there is 12-fold symmetry, 
+#	These coordinates are used to fill a dictionary with the (x,y) pair, and the lipid ID (arbitrarily defined); there is 12-fold symmetry,
 #	therefore there will be 12 "1st-lipid"'s. This dictionary will be used to evaluate where the lipids are at any frame of a trajectory.
-
 	set	center_file	[open $infile r]
-
 	set	centers_read	[read -nonewline $center_file]
-
 	close	$center_file
-
 	set	centers		[split $centers_read "\n"]
-
 	set	DenNum		[expr [llength $centers]]
-
 	puts	"DenNum: $DenNum"
-
 	set	i		1
-
 	foreach line $centers {
-
 		dict set LipDict		$i	x	[lindex $line 0]
 		dict set LipDict		$i	y	[lindex $line 1]
-		dict set LipDict		$i	id	[lindex $line 2] 
+		dict set LipDict		$i	id	[lindex $line 2]
 		dict set LipDict		$i	Zmin	[lindex $line 3]
 		dict set LipDict		$i	Zmax	[lindex $line 4]
 		dict set LipDict		$i	Isolow	[lindex $line 5]
-
 		incr i
 	}
-
 	return [list $LipDict $DenNum]
-
 }
 
 proc get_lipid_list	{} {
-
-#	This proc() generates a list of lipids that ever come within a certain distance of the protein. This is done in order to 
-#	limit the number of unnecessary calculations made. It checks every frame of the .dcd trajectory, selects lipids within the 
+#	This proc() generates a list of lipids that ever come within a certain distance of the protein. This is done in order to
+#	limit the number of unnecessary calculations made. It checks every frame of the .dcd trajectory, selects lipids within the
 #	distance and saves a list, containing the indices of the phosphates.
-
 	global NumFrames Phosp_Ind
-
 	set Phosp_List	""
-
 	for {set n 0} {$n < $NumFrames} {incr n} {
-
 		animate goto $n
-
 #		The range that lipids are selected should be toggled (i.e. between 5 - 20) to make sure that you are not processing
 #		an unnecessary number of calculations for a given job.
-
 		set lipid	[atomselect top "resname DMPC and same residue as within 10 of (protein and resid 84 215)"]
-
 		$lipid			set beta 1
-
 		set	hold		[[atomselect top "name P and beta = 1"] get index]
-
 		set	Phosp_List	[concat [lindex $Phosp_List] [lindex $hold]]
-
 		unset	hold
 	}
-
 	set Phosp_Ind	[lsort -unique $Phosp_List]
 }
 
 proc lip_analysis	{difsel} {
-
 	global Phosp_Ind NumFrames IsoLow LipList
-
-	if {!$difsel} { 
-	
+	if {!$difsel} {
 		set tail_1_text "lipid and (name C22 to C29 C210 to C214)"
 		set tail_2_text "lipid and (name C32 to C39 C310 to C314)"
-		
 	} elseif {$difsel} {
-
 		set tail_1_text "lipid and (name C22 to C24)"
 		set tail_2_text "lipid and (name C32 to C34)"
 	}
-
 	set ind_percent [expr {round([llength $Phosp_Ind] / ([expr [llength $Phosp_Ind] / 20]))}]
-		
 	set i		0
-
 	foreach index $Phosp_Ind {
-
 		set ResID	[[atomselect top "index $index"] get resid]
 		set SegID	[[atomselect top "index $index"] get segid]
-		
 		set tail_1	[atomselect top "resid $ResID and segid $SegID and $tail_1_text"]
 		set tail_2	[atomselect top "resid $ResID and segid $SegID and $tail_2_text"]
-		
 		for {set n 0} {$n < $NumFrames} {incr n} {
-
 			animate goto $n
-
-			set	LipCenterList_1	[which_center	$tail_1] 				
+			set	LipCenterList_1	[which_center	$tail_1]
 			set	LipCenterList_2	[which_center	$tail_2]
-
 			set	LipDen_1	[lindex $LipCenterList_1 0]
 			set	LipID_1		[lindex $LipCenterList_1 1]
 			set	LipDen_2	[lindex $LipCenterList_2 0]
 			set	LipID_2		[lindex $LipCenterList_2 1]
-
-
 			if		{$IsoLow != "none"} {
-
 				set	LipOccupy_1	[eval_density	$tail_1 $LipDen_1]
 				set	LipOccupy_2	[eval_density	$tail_2 $LipDen_2]
-				
 				if	{[lindex $LipOccupy_1 0] && [lindex $LipOccupy_2 0] && ($LipID_1 != $LipID_2)} {
-					
 					pop_matrix $LipID_1 $LipID_2
-
 					set     attr    [list $ResID $SegID $LipID_1 $LipID_2 $n [lindex $LipOccupy_1 1] [lindex $LipOccupy_2 1]]
-
 					lappend	LipList	$attr
-
 					unset	attr
-
 				} else	{variable donothing 0}
-
 			} elseif	{$IsoLow == "none"} then	{pop_matrix $LipID_1 $LipID_2}
-	
 		}
-
 		if {($i % $ind_percent) == 0} {puts -nonewline "*"}
-
 		incr	i
 	}
-
 	puts "*"
 }
 
 proc which_center	{lipid_tail} {
-
 	global LipDict
-	
-	set hold	1000000 
-
+	set hold	1000000
 	set tail_COM	[measure center $lipid_tail]
-
 	set tail_x	[lindex $tail_COM	0]
-
 	set tail_y	[lindex $tail_COM	1]
-
 	foreach DEN	[dict keys $LipDict] {
-
 		set den_x	[dict get $LipDict $DEN x]
 		set den_y	[dict get $LipDict $DEN y]
-
-		set dist	[expr {sqrt(pow(($tail_x - $den_x),2) + pow(($tail_y - $den_y),2))}] 
-
+		set dist	[expr {sqrt(pow(($tail_x - $den_x),2) + pow(($tail_y - $den_y),2))}]
 		if {$dist < $hold} {
-
 			set hold	$dist
-
 			set LipID	[dict get $LipDict $DEN id]
-			
 			set LipDen	$DEN
-
 		} else {	set hold	$hold
-
 		}
 	}
-
 	return	[list $LipDen $LipID]
 }
 
 proc eval_density	{lipid_tail lip_center} {
 #
 #	Currently this proc() takes the list of carbons in the atom selection, creates a list of the carbons indices and finds the
-#	the density value that it occupies. It just calculates what the average density of the carbon tail is...instead I need it 
-#	to calculate how many of the carbons are inside the density. This will return true if the number of carbons is greater than 
+#	the density value that it occupies. It just calculates what the average density of the carbon tail is...instead I need it
+#	to calculate how many of the carbons are inside the density. This will return true if the number of carbons is greater than
 #	the required minimum.
-#	
+#
 #	In addition to IsoLow, this classifier will need to know the minimum number of carbons (MinNum) to qualify for designation.
 #
-#	For every "true" hit, the lipid ID (RESID, not the carbon index val) and the frame of the .dcd file will be saved and stored 
+#	For every "true" hit, the lipid ID (RESID, not the carbon index val) and the frame of the .dcd file will be saved and stored
 #	in an output file.
 
 	global IsoLow LipDict MinCarbon
-
 	set lipid_index		[$lipid_tail get index]
-
 	set tot_den		0
-
 	set NumCarbon		0
-
 	foreach ind $lipid_index {
-
 		set Z_min	[dict get $LipDict $lip_center Zmin]
-
 		set Z_max	[dict get $LipDict $lip_center Zmax]
-
 		set lip_atom	[atomselect top "index $ind"]
-
 		set atom_den	[$lip_atom get interpvol0]
-
 		set atom_center	[lindex [which_center $lip_atom] 0]
-
 		set atom_Z	[lindex [measure center $lip_atom] 2]
-
 		if {$IsoLow == "sep"} {set IsoThresh [dict get $LipDict $lip_center Isolow]} else {set IsoThresh $IsoLow}
-
 		if {($atom_den >= $IsoThresh) && ($atom_center == $lip_center) && (($atom_Z >= $Z_min) &&  ($atom_Z <= $Z_max))} {incr NumCarbon}
 	}
-	
-	if {$NumCarbon >= $MinCarbon} {	
-
+	if {$NumCarbon >= $MinCarbon} {
 		return [list true $NumCarbon]
-
 	} else	{
-
 		return false
 	}
-
 }
 
 proc pop_matrix		{den_id_1 den_id_2 {writematrix false} {outfile false}} {
-
 	global LipMat LipArr DenNum
-
 	if {$writematrix == false} {
-
 		set	den_mat_1	[expr $den_id_1 - 1]
 		set	den_mat_2	[expr $den_id_2 - 1]
-
 		LipMat set cell $den_mat_1 $den_mat_2 [expr $LipArr($den_mat_1,$den_mat_2) + 1]
-
 		LipMat set cell $den_mat_2 $den_mat_1 [expr $LipArr($den_mat_2,$den_mat_1) + 1]
-
 	} elseif {$writematrix == true} {
-
 		set outf	[open $outfile w]
-
 	# row: j, column: i ... tcl matrix object has the form (column,row) which is distinct from matrices in python
-
 		for {set j 0} {$j < $DenNum} {incr j} {
-	
 			for {set i 0} {$i < $DenNum} {incr i} {
-
 				set	val	$LipArr($i,$j)
-
 				puts	-nonewline	$outf	"$val\t"
 			}
-
 			puts	$outf	"\n"
-		} 
-
+		}
 		close	$outf
 	}
-	
 }
