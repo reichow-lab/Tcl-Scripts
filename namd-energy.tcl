4c3c8bdd0db42e4229e5ce4e8ea4a5c48133d0f4
Bassam Haddad <bhaddad@pdx.edu>
2020-07-12 17:49:16 -0700

    fixed ff path

--- namd-energy.tcl
+++ namd-energy.tcl
@@ -3,5 +3,5 @@ puts "To run namdenergy type the following: Calc_Energy 'outfile name' 'time-ste
 proc Calc_Energy {ofile temp timemult sel1 sel2} {
 
 	package require namdenergy
-	namdenergy -elec -vdw -nonb -sel $sel1 $sel2 -T 310 -ofile $ofile -tempname $temp -diel 1 -timemult $timemult -stride 1 -par /home/bassam/Lab_Files/Topology\ and\ Parameters/par_all36_carb.prm -par /home/bassam/Lab_Files/Topology\ and\ Parameters/par_all36_cgenff.prm -par /home/bassam/Lab_Files/Topology\ and\ Parameters/par_all36_lipid.prm -par /home/bassam/Lab_Files/Topology\ and\ Parameters/par_all36m_prot.prm -par /home/bassam/Lab_Files/Topology\ and\ Parameters/toppar_water_ions_namd.str
+	namdenergy -elec -vdw -nonb -sel $sel1 $sel2 -T 310 -ofile $ofile -tempname $temp -diel 1 -timemult $timemult -stride 1 -par /home/bassam/Topology\ and\ Parameters/par_all36_carb.prm -par /home/bassam/Topology\ and\ Parameters/par_all36_cgenff.prm -par /home/bassam/Topology\ and\ Parameters/par_all36_lipid.prm -par /home/bassam/Topology\ and\ Parameters/par_all36m_prot.prm -par /home/bassam/Topology\ and\ Parameters/toppar_water_ions_namd.str
 }
