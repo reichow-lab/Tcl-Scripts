98f11bd83af6dc780ef78427be9a706e573064f9
Bassam Haddad <ghaddad.bassam@gmail.com>
2020-11-27 18:10:40 -0800

    fix

--- IonMobile.tcl
+++ IonMobile.tcl
@@ -1,9 +1,13 @@
 # The purpose of this script it to calculate the height dependent diffusion coefficient of an Ion along the pore-axis (z-axis). By generating the distribution
 # of single steps we can calculate the MSD (i.e. variance) and from that we can approximate diffusion.
+source ~/Scripts/TCL/Tcl-Scripts/auto-ionz-IN.tcl
 puts "Load molecule prior to running."
 puts "To run IonMobile.tcl, type: imob <ION> <window size> <outname>"
 
 proc imob {ION WS outname} {
+	set ID [molinfo top get id]
+	align $ID $ID
+	set protz [lindex [measure center [atomselect top protein]] 2]
 	# set the system size and subdivide into windows
 	set sys [atomselect top "all"]
 	set zmin [lindex [measure minmax $sys] 0 2]
@@ -12,10 +16,9 @@ proc imob {ION WS outname} {
 	set out [open $outname.txt w]
 	# loop through windows
 	for {set i 0} {$i <= $WN} {incr i} {
-		puts $out "BinCenter:  [expr ((($zmin + ($WS * $i)) + ($zmin + ($WS * ($i + 1)))) / 2) - [lindex [measure center [atomselect top protein]] 2]]"
+		puts $out "BinCenter:\t[expr ((($zmin + ($WS * $i)) + ($zmin + ($WS * ($i + 1)))) / 2) - $protz]"
 		# loop through frames
 		for {set j 0} {$j < [molinfo top get numframes]} {incr j} {
-			set protz [lindex [measure center [atomselect top protein]] 2]
 			animate goto $j
 			set ionlist [atomselect top "segname ION and name $ION and (z > [expr $zmin + ($WS * $i) - $protz] and z < [expr $zmin + ($WS * ($i + 1)) - $protz])"]
 			set indlist [$ionlist get index]
