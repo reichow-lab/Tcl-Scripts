831d40d0a80b98ae297f1548eff6ba20cc4bf067
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-05-16 09:01:48 -0700

    new function in WatRes.tcl, needs work

--- WatRes.tcl
+++ WatRes.tcl
@@ -5,10 +5,10 @@ proc watres {outname CAT} {
   puts $out "Frame\tPoreRatio\tBulkRatio"
   for {set i 0} {$i < $numf} {incr i} {
     animate goto $i
-    set pore    [atomselect top "water and same residue as ((abs(z) < 70) and (x^2 + y^2 < 500))"]
-    set poreIon [atomselect top "water and same residue as ((abs(z) < 70) and (x^2 + y^2 < 500) and within 20 of (name $CAT))"]
-    set bulk    [atomselect top "water and same residue as (abs(z) > 70)"]
-    set bulkIon [atomselect top "water and same residue as ((abs(z) > 70) and within 20 of (name $CAT))"]
+    set pore    [atomselect top "water and name OH2 ((abs(z) < 70) and (x^2 + y^2 < 500))"]
+    set poreIon [atomselect top "water and name OH2 ((abs(z) < 70) and (x^2 + y^2 < 500) and within 20 of (name $CAT))"]
+    set bulk    [atomselect top "water and name OH2 (abs(z) > 70)"]
+    set bulkIon [atomselect top "water and name OH2 ((abs(z) > 70) and within 20 of (name $CAT))"]
     set numPore     [$pore num]
     set numPoreIon  [$poreIon num]
     set numBulk     [$bulk num]
@@ -23,4 +23,10 @@ proc watres {outname CAT} {
     #unset numPore, numPoreIon, numBulk, numBulkIon, PoreRatio, BulkRatio
   }
   close $out
+}
+proc catoccupy {outname CAT isoform} {
+  """ Calculate the occupancy of the transient binding sites throughout the simulation.
+      First I need to define the binding sites, then just moniter their occupancy by frame.
+      Each isoform may have specific residues (i.e., binding sites) of interest.
+  """
 }
