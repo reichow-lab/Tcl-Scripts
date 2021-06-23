64a6e164facbe483a8bedb3cc5ffda1808a720b7
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-06-23 13:35:27 -0700

    test

--- OrderByCurrent.tcl
+++ OrderByCurrent.tcl
@@ -26,7 +26,9 @@ proc orderbycurrent {TextIN PSFin DCDinList peaklist outname st} {
   foreach from $peakFROM to $peakTO bin $BinN {
     foreach line $DATA {
       if {[string is double -strict [lindex $line 1]]} {
+        puts "debug-0"
         if {[lindex $line 1] <= [expr [lindex $peakFROM 0]]} {
+          puts "debug-0.5"
           lset BINS 0 end+1 [expr int([lindex $line 0])]
           puts "debug-1"
         } elseif {([lindex $line 1] > $from) && ([lindex $line 1] <= $to)} {
