9a3ab0a3f8c88271a68698d630a5de594ea40c39
Bassam <ghaddad.bassam@gmail.com>
2021-06-22 17:44:20 -0700

    Now allows arbitrary number of "current-states"

--- OrderByCurrent.tcl
+++ OrderByCurrent.tcl
@@ -3,38 +3,41 @@ puts "create a list of the min-mid & mid-max boundaries"
 proc orderbycurrent {TextIN PSFin DCDinList peaklist outname st} {
   set INFILE [open $TextIN r]
   set INDATA [read $INFILE]
+  set last [expr [llength $peaklist] - 1]
+  set peakFROM [lreplace $peaklist $last $last]
+  set peakTO  [lreplace $peaklist 0 0]
   close $INFILE
   set DATA [split $INDATA "\n"]
-  # read through the data and find the maximum Current
-  set max 0
-  foreach line $DATA {
-    if {[string is double -strict [lindex $line 1]]} {
-      if {[expr abs([lindex $line 1])] > [expr abs($max)]} {set max [lindex $line 1]}
-    }
+  # peakFROM and peakTO are the boundaries between "current-states", and allow
+  # for an arbitrary number of "current-states" than simply min mid & max
+  # Generate list of Bins. BINS is initialized with the min and max bins, middle
+  # bins are added in the next loop
+  set BINS "{} {}"
+  for {set i 1} {$i < [expr [llength $peakFROM] - 1]} {incr i} {
+    lappend BinN $i
+    lappend BINS ""
   }
-  # simply split into 3 groups: min mid max
-  set minmid [lindex $peaklist 0]
-  set midmax [lindex $peaklist 1]
   # generate list of frames and their assignments
-  set Bin0 {}
-  set Bin1 {}
-  set Bin2 {}
-  foreach line $DATA {
-    if {[string is double -strict [lindex $line 1]]} {
-      if {[lindex $line 1] <= [expr $minmid]} {
-        lappend Bin0 [expr int([lindex $line 0])]
-      } elseif {([lindex $line 1] > $minmid) && ([lindex $line 1] <= $midmax)} {
-        lappend Bin1 [expr int([lindex $line 0])]
-      } elseif {[lindex $line 1] > $midmax} {
-        lappend Bin2 [expr int([lindex $line 0])]
+  foreach from $peakFROM to $peakTO bin $BinN {
+    foreach line $DATA {
+      if {[string is double -strict [lindex $line 1]]} {
+        if {[lindex $line 1] <= [expr [lindex $peakFROM 0]]} {
+          lset BINS 0 end+1 [expr int([lindex $line 0])]
+        } elseif {([lindex $line 1] > $from) && ([lindex $line 1] <= $to)} {
+          lset BINS $bin end+1 [expr int([lindex $line 0])]
+        } elseif {[lindex $line 1] > [lindex $peakTO [expr [llength $peakTO] - 1]} {
+          lset BINS [expr [llength $BINS] - 1] end+1 [expr int([lindex $line 0])]
+        }
       }
     }
   }
-  # rearrange the order of the assigned frame_list
-  set BINS [list $Bin0 $Bin1 $Bin2]
+  # sort bin lists to remove redundant frame-IDs
+  for {set i 0} {$i < [llength $BINS]} {incr i} {
+    lappend BINSort [lreplace $BINS 0 [expr [llength $BINS] - 1] [lsort -unique [lindex $BINS $i]]]
+  }
   # Now that the frames are ordered, properly order the dcd
   set b 0
-  foreach Bin $BINS {
+  foreach Bin $BINSort {
     puts "$Bin"
     mol new $PSFin
     foreach dcd $DCDinList {
