1599d61c7f347c49ccd2510ea9c9da1d8dc23794
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-06-23 13:19:31 -0700

    test

--- OrderByCurrent.tcl
+++ OrderByCurrent.tcl
@@ -14,11 +14,10 @@ proc orderbycurrent {TextIN PSFin DCDinList peaklist outname st} {
   # bins are added in the next loop
   set BINS "{} {}"
   set BinN ""
-  for {set i 1} {$i < [expr [llength $peaklist] - 1]} {incr i} {
+  for {set i 1} {$i <= [expr [llength $peaklist] - 1]} {incr i} {
     lappend BinN $i
     lappend BINS ""
   }
-  puts "$BinN"
   # generate list of frames and their assignments
   foreach from $peakFROM to $peakTO bin $BinN {
     foreach line $DATA {
