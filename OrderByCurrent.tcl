169d32ee41e380c35436fe2d438c212640f8cf24
Bassam Haddad <ghaddad.bassam@gmail.com>
2021-06-23 16:09:34 -0700

    test with 4 static bins

--- OrderByCurrent.tcl
+++ OrderByCurrent.tcl
@@ -1,5 +1,5 @@
 puts "create a list of dcd's that you want processed."
-puts "create a list of the min-mid & mid-max boundaries"
+puts "create a list of the min-mid & mid-max boundaries...must have 3 values"
 proc orderbycurrent {TextIN PSFin DCDinList peaklist outname st} {
   set INFILE [open $TextIN r]
   set INDATA [read $INFILE]
@@ -10,41 +10,32 @@ proc orderbycurrent {TextIN PSFin DCDinList peaklist outname st} {
   set DATA [split $INDATA "\n"]
   # peakFROM and peakTO are the boundaries between "current-states", and allow
   # for an arbitrary number of "current-states" than simply min mid & max
-  # Generate list of Bins. BINS is initialized with the min and max bins, middle
-  # bins are added in the next loop
-  set BINS "{} {}"
+  # Generate a number of Bins.
   set BinN ""
-  for {set i 1} {$i <= [expr [llength $peaklist] - 1]} {incr i} {
+  for {set i 0} {$i <= 3} {incr i} {
     lappend BinN $i
-    lappend BINS ""
+    set Bin_$i ""
   }
   puts $BinN
-  puts $BINS
   puts $peakFROM
   puts $peakTO
   # generate list of frames and their assignments
   foreach from $peakFROM to $peakTO bin $BinN {
     foreach line $DATA {
       if {[string is double -strict [lindex $line 1]]} {
-        puts "debug-0"
         if {[lindex $line 1] <= [expr [lindex $peakFROM 0]]} {
-          puts "debug-0.5"
-          lset BINS 0 end+1 [expr int([lindex $line 0])]
-          puts "debug-1"
+          lappend Bin_0 [expr int([lindex $line 0])]
         } elseif {([lindex $line 1] > $from) && ([lindex $line 1] <= $to)} {
-          puts "debug-1.5"
-          lset BINS $bin end+1 [expr int([lindex $line 0])]
-          puts "debug-2"
+          lappend Bin_$bin [expr int([lindex $line 0])]
         } elseif {[lindex $line 1] > [lindex $peakTO [expr [llength $peakTO] - 1]} {
-          puts "debug-2.5"
-          lset BINS [expr [llength $BINS] - 1] end+1 [expr int([lindex $line 0])]
-          puts "debug-3"
+          lappend Bin_[expr [llength $peaklist] - 1] [expr int([lindex $line 0])]
         }
       }
     }
   }
   puts "debug-4"
-  # sort bin lists to remove redundant frame-IDs
+  set BINS [list $Bin_0 $Bin_1 $Bin_2 $Bin_3]
+  # sort bin lists to remove redundant "" frame-IDs
   for {set i 0} {$i < [llength $BINS]} {incr i} {
     lappend BINSort [lreplace $BINS 0 [expr [llength $BINS] - 1] [lsort -unique [lindex $BINS $i]]]
   }
